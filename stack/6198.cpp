34411c8040851e5893bbe0dd1ac4f949000ebd5c
SJH <wlgjs8474@naver.com>
2022-07-17 23:06:23 +0900

    2022-07-17

---
+++ stack/6198.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int stack[80001];
+    for (int i = 0; i < n; i++) {
+        cin >> stack[i];
+    }
+    stack[n] = 1000000001;
+
+    int idx = 0;
+    int sum = 0;
+    while(idx < n) {
+        int curHeight = stack[idx];
+        int tmp = idx+1;
+        while(true) {
+            int nextHeight = stack[tmp];
+
+            if (curHeight > nextHeight) tmp++;
+            else if (curHeight == nextHeight) {
+                tmp++;
+                break;
+            }
+            else { // curHeight < nextHeight
+                break;
+            }
+
+            stack[idx] = tmp-idx-1;
+            sum += tmp-idx-1;
+        }
+        idx++;
+    }
+    cout << sum;
+}
