4a741345306c68d94dcb4fdfa439e4865bbdb174
SJH <wlgjs8474@naver.com>
2023-08-14 19:58:05 +0900

    부분 수열 3문제 해결

--- DP/11722.cpp
+++ DP/11722.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int mem[1001][2];
-// mem[i][0]: input 값
-// mem[i][1]: 각 수가 취할 수 있는 가장 큰 증가하는 부분 수열 값
 
 int main() {
     cin.tie(0) -> sync_with_stdio(false);
 
+
     int n;
     cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        fill(mem[i], mem[i] + 2, 1);
+    }
+
     for (int i = 0; i < n; i++) {
         cin >> mem[i][0];
     }
 
     int res = 0;
     for (int i = 0; i < n; i++) {
-        mem[i][1] = mem[i][0];
         for (int j = i - 1; j >= 0; j--) {
-            if (mem[j][0] < mem[i][0]) {
-                mem[i][1] = max(mem[i][1], mem[j][1] + mem[i][0]);
-            }
+            if (mem[j][0] > mem[i][0]) {
+                mem[i][1] = max(mem[i][1], mem[j][1] + 1);
+            }  
         }
         res = max(res, mem[i][1]);
     }
-
     printf("%d\n", res);
 }
